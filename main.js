0ff284ad36ad385e1a78bdf31f431da637cd1da0
Karolina Charlinska <karolina.charlinska@gmail.com>
2019-05-26 15:40:31 +0200

    Store all required data

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ fetch('https://www.reddit.com/r/funny.json')
     })
     .then(data => {
         console.log(data)
+        const postsData = new Object();
+        postsData.posts = [];
+        data.data.children.forEach(item => {
+            postsData.posts.push({
+                title: item.data.title,
+                upvotes: item.data.ups,
+                score: item.data.score,
+                num_comments: item.data.num_comments,
+                created: new Date(item.data.created_utc * 1000)
+            })
+        });
+        postsData.count = postsData.posts.length;
+        console.log(postsData);
     })
     .catch(error => {
         if (error.status === 404) {
